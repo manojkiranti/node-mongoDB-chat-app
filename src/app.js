ffa4986532ee220d5b66eb954d13b7c2d0a11bd5
NICASIA <manoj.kiranti@gmail.com>
2024-02-06 21:05:55 +0545

    middleware added

--- src/app.js
+++ src/app.js
@@ -1,9 +1,39 @@
 import express from "express";
+import dotenv from "dotenv";
+import morgan from "morgan";
+import helmet from "helmet";
+import ExpressMongoSanitize from "express-mongo-sanitize";
+import cookieParser from "cookie-parser";
+import compression from "compression";
 
-//create express app
+// dotEnv config
+dotenv.config();
+
+// create express app
 const app = express();
 
-app.get("/", (req, res) => {
+// middleware
+app.use(helmet());
+
+// morgan.middleware
+if (process.env.NODE_ENV !== 'production') {
+    app.use(morgan(':method :url :status :res[content-length] - :response-time ms'));
+}
+// parse json request
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// sanitize request data    
+app.use(ExpressMongoSanitize());
+
+// enable cookies parser
+app.use(cookieParser());
+
+// gzip compression
+app.use(compression());
+
+app.post("/", (req, res) => {
+    console.log(req.body)
     res.send("Hello from the server");
 });
 
